86cd63837776d44a96262fca0e55aa47d8490d0f
mohamedrxo <mohamedrxo4@gmail.com>
2026-04-16 22:39:37 +0100

    adding tqdm and matplotlib to track progress

--- model/trainer.py
+++ model/trainer.py
@@ -1,7 +1,8 @@
 import torch.nn as nn
 import torch
 from torch.utils.data import DataLoader
-
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 class Trainer(nn.Module):
     def __init__(self,config,model,data):
@@ -9,18 +10,43 @@ class Trainer(nn.Module):
         self.config = config
         self.model = model
         self.data = DataLoader(data,shuffle=config.shuffle,batch_size=config.batch_size)
-    
-    def train(self):
+        self.loss_history = []
 
-        optimizer = torch.optim.Adam(self.model.parameters(),lr=self.config.lr)
+    def train(self, use_tqdm=False):
 
-        for _ in range(self.config.epochs):
+        self.loss_history = []
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config.lr)
+
+        for epoch in range(self.config.epochs):
             
-            for x,y in self.data:
+            if use_tqdm:
+                pbar = tqdm(self.data, desc=f"Epoch {epoch+1}/{self.config.epochs}")
+                loader = pbar
+            else:
+                loader = self.data
 
-                _, loss =self.model(x,y)
+            for x, y in loader:
+
+                _, loss = self.model(x, y)
+
                 optimizer.zero_grad()
                 loss.backward()
+
                 torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+
                 optimizer.step()
-                print("Loss: ",loss.item())
+
+                self.loss_history.append(loss.item())
+
+                if use_tqdm:
+                    pbar.set_postfix(loss=loss.item())
+                else:
+                    print("Loss:", loss.item())
+
+    def plot_loss(self):
+            plt.figure()
+            plt.plot(self.loss_history)
+            plt.title("Loss progression")
+            plt.xlabel("Steps")
+            plt.ylabel("Loss")
+            plt.show()
